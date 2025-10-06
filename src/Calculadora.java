f019cdadc9fabcfcbb69fa5bee2aa6b62f65dc0a
pwyll <diascamara38@gmail.com>
2025-10-06 14:26:11 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,40 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int soma(int val1, int val2) {
+		return val1 + val2;
+	}
+	public static double soma(double val1, double val2) {
+		return val1 + val2;
+	}
+	public static int mult(int val1, int val2) {
+		return val1 * val2;
+	}
+	public static int sub(int val1, int val2) {
+		return val1 - val2;
+	}
+	public static int div(int val1, int val2) {
+		return val1 / val2;
+	}
+	public static double div(double val1, double val2) {
+		return val1 / val2;
+	}
+	
+	
+	
+	
+	
+	
+	
+//	public double soma(double a, double b) {
+//		return a + b;
+//	}
+//	public double div(double a, double b) {
+//		return a / b;
+//	}
+
+}
